152de7fb7926f57a3376c17785e82f48fdb6dc4a
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-08-13 01:22:04 -0400

    Add default prop to Display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Display extends React.Component { /* eslint-disable-line react/prefer-stateless-function */
   render() {
+    const { value } = this.props; /* eslint-disable-line react/prop-types */
+
     return (
-      <div className="Display">0</div>
+      <div className="Display">{value}</div>
     );
   }
 }
+
+Display.propsType = {
+  value: PropTypes.number.isRequired,
+};
+
+Display.defaultProps = {
+  value: 0,
+};
 
 export default Display;
